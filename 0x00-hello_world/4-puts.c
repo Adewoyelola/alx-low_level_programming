e79574f964ecf224a778ca7daf1663275bfe38b2
Adewoyelola <adewoyelola@gmail.com>
2023-06-15 06:38:10 -0700

    added puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -A programme that prints a line with puts function
+ * return: 0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
