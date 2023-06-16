2a87799d1bce8e989dc2d626bf2d348153d5422a
Adewoyelola <adewoyelola@gmail.com>
2023-06-16 11:51:41 -0700

    added 4 aplhabt file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,19 +9,18 @@
  */
 int main(void)
 {
-	char letter;
+	char letter, e, q;
 
-	for (letter = 'a'; letter <= 'z'; letter)
+	e = 'e';
+	q = 'q';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'q' || letter != 'e')
+		if (letter != q && letter != e)
 		{
 			putchar(letter);
 		}
-		else
-		{
-			putchar('\n');
-		}
 	}
-
+	putchar('\n');
 	return (0);
 }
