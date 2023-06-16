fde505d89bd73b18b9d94ad36da04d2a88ca909d
Adewoyelola <adewoyelola@gmail.com>
2023-06-16 13:24:34 -0700

    added print comb3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,17 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		putchar(',');
 		putchar(' ');
+
+		if ( a == 9)
+		{
+			putchar('\n');
+		}
 	}
-	putchar('\n');
 
 	return (0);
 }
