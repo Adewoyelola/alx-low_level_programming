2c7c68607e88a090bc9e76a55832e35df4e3a61b
Adewoyelola <adewoyelola@gmail.com>
2023-06-16 11:31:30 -0700

    3 -print alphabetd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	int j;
+	for (j = 65; j <= 90; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
