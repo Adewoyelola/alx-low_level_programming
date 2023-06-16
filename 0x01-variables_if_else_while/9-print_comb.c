ae7e3ab6c36c16df21733bc45423566c30bb0779
Adewoyelola <adewoyelola@gmail.com>
2023-06-16 12:50:22 -0700

    added 9 print comb file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program to print combination
+ * Return: Always 0 (Success);
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+
+		if (a != 9 && a < 10)
+		{
+			putchar('\n');
+		}
+	}
+
+	return (0);
+}
